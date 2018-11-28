704800d10896a952d1ac5346f159253ff4151128
Biakp <42621272+biakp@users.noreply.github.com>
2018-11-27 23:23:42 -0200

    Update app.js

--- app.js
+++ app.js
@@ -74,19 +74,21 @@ function closeCard() {
 
 //Match cards
 function matchCards() {
-    if (openCards.length === 2) {
-      $(".deck").addClass("disabled");
-      if (openCards[0].classList[2] === openCards[1].classList[2]) {
-        openCards[0].classList.add("match");
-        openCards[1].classList.add("match");
-        $(openCards[0]).addClass("disabled animated shake");
-        $(openCards[1]).addClass("disabled animated shake");
-        win++;
-      } else {
-        setTimeout(closeCard, 3000);
-      }
+  if (openCards.length === 2) {
+    $(".deck").addClass("disabled");
+    if (openCards[0].classList[2] === openCards[1].classList[2]) {
+      openCards[0].classList.add("match");
+      openCards[1].classList.add("match");
+      $(openCards[0]).addClass("disabled animated shake");
+      $(openCards[1]).addClass("disabled animated shake");
+      $(".deck").removeClass("disabled");
+      openCards = [];
+      win++;
+    } else {
+      setTimeout(closeCard, 3000);
     }
   }
+}
 
 //Timer
 function timing() {
